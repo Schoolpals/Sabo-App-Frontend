9bf58137cbeeff1e15a208fd5fd29213eb9d529e
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-04-26 10:50:21 +0100

    format sign in

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
-import {Link} from "react-router-dom"
+import React, { useState } from 'react';
+import { Link } from "react-router-dom"
 
 // type SignInProps = {
-    
+
 // };
 
 const SignIn: React.FC = () => {
@@ -19,7 +19,7 @@ const SignIn: React.FC = () => {
     } else {
       setEmailError('');
     }
-     
+
     if (!email.trim() || !password.trim()) {
       setFormError('Please fill in both email and password');
       return;
@@ -30,78 +30,77 @@ const SignIn: React.FC = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
- setPasswordVisible(!passwordVisible);
+    setPasswordVisible(!passwordVisible);
   }
 
   const validateEmail = (email: string) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   };
-  
-    return (
-        <>
-        <Link to="./sign-up">
-        <img src="/assets/images/arrowback.png" alt=""  className='ml-0 mt-1'/>
-        </Link>
+
+  return (
+    <>
+      <Link to="./sign-up">
+        <img src="/assets/images/arrowback.png" alt="" className='ml-0 mt-1' />
+      </Link>
       <div className=" flex justify-center items-center ">
         {/* Content of the Sign In page */}
         <div className="p-0">
           <h2 className="text-2xl font-bold mb-20 justify-center mt-6 flex text-white">Sign In to your Sabo Account</h2>
-          
+
           <div className='relative w-full mb-4'>
             <img src='/assets/images/Group.png'
-            alt='email-icon'
-            className='absolute left-3 top-7 transform -translate-y-1/2 h-5 w-5'
+              alt='email-icon'
+              className='absolute left-3 top-7 transform -translate-y-1/2 h-5 w-5'
             />
-                 <input type="email" className="w-full mb-4 px-4 py-4 p-2 placeholder-gray-400 border rounded-lg text-gray-100	
- bg-blue-950"  placeholder="Email" 
-  style={{ border: 'none', paddingLeft: '45px',  paddingRight:'40px'}}
-  value={email}
-  onChange={(e) => setEmail(e.target.value)}
-  />
-   {emailError && <p className="text-red-500">{emailError}</p>}
+            <input type="email" className="w-full mb-4 px-4 py-4 p-2 placeholder-gray-400 border rounded-lg text-gray-100	
+ bg-blue-950"  placeholder="Email"
+              style={{ border: 'none', paddingLeft: '45px', paddingRight: '40px' }}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailError && <p className="text-red-500">{emailError}</p>}
 
           </div>
-     
+
           <div className='relative w-full mb-4'>
             <img src='/assets/images/password.png'
-            alt='password-icon'
-            className='absolute left-3 top-7 transform -translate-y-1/2 h-5 w-5'
+              alt='password-icon'
+              className='absolute left-3 top-7 transform -translate-y-1/2 h-5 w-5'
             />
-                 <input type={passwordVisible ? "text" :"password"} className="w-full mb-4 px-4 py-4 p-2 placeholder-gray-400 border rounded-lg text-gray-100	
- bg-blue-950"  placeholder="Password" 
-  style={{ border: 'none', paddingLeft: '45px', paddingRight: '40px'}}
-  value={password}
-  onChange={(e) => setPassword(e.target.value)}
-  />
-  <img onClick={togglePasswordVisibility} className='relative bottom-12  mr-3 flex float-end' src="/assets/images/eye.png" alt="" />
+            <input type={passwordVisible ? "text" : "password"} className="w-full mb-4 px-4 py-4 p-2 placeholder-gray-400 border rounded-lg text-gray-100	
+ bg-blue-950"  placeholder="Password"
+              style={{ border: 'none', paddingLeft: '45px', paddingRight: '40px' }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <img onClick={togglePasswordVisibility} className='relative bottom-12  mr-3 flex float-end' src="/assets/images/eye.png" alt="" />
           </div>
           {formError && <p className="text-red-500 mb-4">{formError}</p>}
 
           <div className="flex justify-center  items-center mb-4">
             {/* <img src="/assets/images/Rectangle.png" alt="checkBox" /> */}
-            
+
             <input type="checkbox" className=" cursor-pointer mr-2 bg-transparent rounded-md w-4 h-5  " />
-             
+
             <p className='text-white cursor-pointer'>Remember me</p>
           </div>
 
           <button
-          onClick={handleSignIn}
-          className="bg-blue-700 w-full text-white px-4 py-4 rounded-lg hover:bg-blue-600 mt-1 ">Sign In</button>
-          <h2  className="mt-4  text-sm text-blue-600 cursor-pointer flex justify-center">
+            onClick={handleSignIn}
+            className="bg-blue-700 w-full text-white px-4 py-4 rounded-lg hover:bg-blue-600 mt-1 ">Sign In</button>
+          <h2 className="mt-4  text-sm text-blue-600 cursor-pointer flex justify-center">
             <Link className='text-lg' to="/forgot-password">Forgot password?</Link></h2>
-         
+
         </div>
-        
+
       </div>
       <h2 className="mt-40 text-lg flex justify-center text-white ">
-            Don't have an account? 
-            <Link to="/sign-up" className="text-blue-500"> Sign up</Link>
-          </h2>
-      </>
-    );
-  };
-  
-  export default SignIn;
-  
+        Don't have an account?
+        <Link to="/sign-up" className="text-blue-500"> Sign up</Link>
+      </h2>
+    </>
+  );
+};
+
+export default SignIn;
